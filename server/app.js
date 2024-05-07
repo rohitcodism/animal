0307d428424f4971dbf8f146072c3dfa4f092d16
rohitcodism <rohitpaula18i@gmail.com>
2024-05-08 01:16:22 +0530

    chore: Configure server to handle CORS and static files, and add animal routes

---
+++ server/app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import animalRouter from './routes/animal.routes.js';
+
+
+
+const app = express();
+
+app.use(cors(
+    {
+        origin: '*',
+        credentials: true,
+    }
+))
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true, limit: '16kb' }));
+
+app.use(express.static('public'));
+
+app.use('/api/v1/animals', animalRouter);
+
+export { app }
---
+++ server/index.js
@@ -0,0 +1,17 @@
+import { connectDb } from "./utils/connectDb.js";
+import { app } from "./app.js";
+
+connectDb()
+.then(() => {
+    app.on("error", (error) => {
+        console.log(`Server error ${error}`);
+        throw error;
+    })
+
+    app.listen(process.env.PORT || 8000,() => {
+        console.log("Server is running on port 8000")
+    })
+})
+.catch((error) => {
+    console.log(`MongoDB connection error ${error}`);
+})
