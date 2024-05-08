9a5fdd12c829dc1ed4dc89600db6d8e2407e5178
rohitcodism <rohitpaula18i@gmail.com>
2024-05-08 09:12:08 +0530

    feat: Add animal types to constants
    This commit adds the `animalTypes` array to the `animals.js` file in the `constants` directory. The array contains various types of animals, each with a value and label. This addition provides a centralized location for managing the available animal types in the application.
    
    Note: Please remove any meta information like issue references, tags, or author names from the commit message.

---
+++ src/constants/animals.js
@@ -0,0 +1,42 @@
+export const animalTypes = [
+    {
+        value: 'mammal',
+        label: 'Mammal',
+    },
+    {
+        value: 'bird',
+        label: 'Bird',
+    },
+    {
+        value: 'reptile',
+        label: 'Reptile',
+    },
+    {
+        value: 'amphibian',
+        label: 'Amphibian',
+    },
+    {
+        value: 'fish',
+        label: 'Fish',
+    },
+    {
+        value: 'invertebrate',
+        label: 'Invertebrate',
+    },
+    {
+        value: 'insect',
+        label: 'Insect',
+    },
+    {
+        value: 'bug',
+        label: 'Bug',
+    },
+    {
+        value: 'fungus',
+        label: 'Fungus',
+    },
+    {
+        value: 'worm',
+        label: 'Worm',
+    },
+];
